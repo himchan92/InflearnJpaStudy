b3df1b918a31252d1685d15f8d5e7c875ae1e6a0
omea92 <thdjun92@naver.com>
2025-03-10 23:42:50 +0900

    연간관계 매핑 엔티티 생성 실습

---
+++ src/main/java/hellojpa/domain/Order.java
@@ -0,0 +1,28 @@
+package hellojpa.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
