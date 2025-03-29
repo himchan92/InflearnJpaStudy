dea3040283871515aebac24e64be93eed9f3ccd6
omea92 <thdjun92@naver.com>
2025-03-29 16:42:05 +0900

    init commit

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,7 +8,16 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
+
+        // JPA 변경작업은 반드시 한트랜젝션내에서 수행되야해서 셋팅필수
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("HelloA");
+        em.persist(member); //저장
+        tx.commit(); //실제 DB 반영처리
 
         em.close();
         emf.close();
