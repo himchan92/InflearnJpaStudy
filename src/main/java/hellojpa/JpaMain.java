08a83b03ecff77a00d802ffe34361b646c41e4ac
omea92 <thdjun92@naver.com>
2025-03-29 17:27:10 +0900

    JPA 영속, 비영속, 커밋 개념 주석정리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class JpaMain {
 
     public static void main(String[] args) {
-
+        //영속성 컨택스트 : DB 트랜젝션 단위로 수행
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
 
@@ -21,14 +21,16 @@ public class JpaMain {
             member.setId(1L);
             member.setName("HelloA");
 
-            //---------- 영속 상태(실제 DB 저장 x) ----------//
-            em.persist(member);
+            //---------- 영속 상태(실제 DB 수행 x), 1차캐시 저장 ----------//
+            //em.persist(member);
 
             //em.detach(member): // 준영속상태로 변경
             //em.remove(member); // 수행대상 삭제
 
             //JPA 조회
-            Member findMember = em.find(Member.class, 1L); //1L 조회
+            Member findMember = em.find(Member.class, 1L); //1차 캐시에서 조회
+            Member findMember2 = em.find(Member.class, 1L); //동일 조회문이나 앞서 1차캐시에서 이미 조회했기에 조회 안됨
+            System.out.println(findMember == findMember2); //동일 보장 TRUE
             System.out.println("findMember id = " + findMember.getId());
             System.out.println("findMember name = " + findMember.getName());
 
@@ -42,10 +44,12 @@ public class JpaMain {
                 System.out.println("item.name = " + item.getName());
             }
 
-            //JPA 수정
-            findMember.setName("HelloJPA");
+            //JPA 수정(변경감지)
+            Member find = em.find(Member.class, 1L);
+            System.out.println("member find = " + find);
+            find.setName("HelloBoot");
 
-            //------ 실제 DB 반영 처리 ------//
+            //------ 실제 DB 수행 ------//
             // 변경감지 체크단계로 위 setter 작업 등 변경 있으면 UPDATE 수행
             tx.commit();
         } catch (Exception e) {
